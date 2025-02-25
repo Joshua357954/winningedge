c953e05feb91e003c1b9d2f5988461ec163103cf
joshua boyi <boyijoshua72@gmail.com>
2025-02-25 21:19:56 +0100

    new

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import Avatar from "./avatar";
 import useUserStore from "@/store/userStore";
@@ -8,20 +8,64 @@ import { Toaster } from "react-hot-toast";
 import Logo from "./Logo";
 
 export default function Navbar() {
-  const { user } = useUserStore();
+  const { user, logout } = useUserStore();
+  const [showPopover, setShowPopover] = useState(false);
+  const popoverRef = useRef(null);
+
+  const handleLogout = () => {
+    if (confirm("Are you sure you want to log out?")) {
+      logout();
+      router.push("/login");
+    }
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (popoverRef.current && !popoverRef.current.contains(event.target)) {
+        setShowPopover(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <header className="header__dashboard header__alt">
+    <header className="header__dashboard header__alt relative">
       <Toaster />
       <nav className="navbar navbar-expand-xl">
         <div className="container">
-          <Logo/>
+          <Logo />
 
-          <div className="navbar__out order-2 order-xl-3">
+          <div className="navbar__out order-2 order-xl-3 relative">
             <div className="dashboard__nav">
-              <button className="profile__small">
+              <button
+                className="profile__small relative"
+                onClick={() => setShowPopover(!showPopover)}
+              >
                 <Avatar name={user?.name} />
               </button>
+              {showPopover && (
+                <div
+                  ref={popoverRef}
+                  className="absolute right-0 top-10 mt-2 w-48 bg-white shadow-lg rounded-lg p-4 z-40"
+                >
+                  {/* <p className="z-50 font-semibold text-gray-800 mb-1">Name:</p> */}
+                  <p className="z-50 font-semibold text-gray-800 mb-4">
+                    {user?.name}
+                  </p>
+
+                  <p className="text-md text-blue-600 mb-1">Email:</p>
+                  <p className="text-sm text-gray-600 mb-2">{user?.email}</p>
+
+                  <hr className="my-2" />
+
+                  <p className="block w-full text-left text-red-500 text-sm mt-2 hover:text-red-600 cursor-pointer" onClick={handleLogout}>
+                    Logout
+                  </p>
+                </div>
+              )}
             </div>
 
             <button
